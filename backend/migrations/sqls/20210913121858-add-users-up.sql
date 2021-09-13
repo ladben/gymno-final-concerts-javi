ead215dc6f5121dae596dde7f80b96b379450551
Ladányi Bence <ladanyi.bence96@gmail.com>
2021-09-13 14:27:05 +0200

    database migration added

---
+++ backend/migrations/sqls/20210913121858-add-users-up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE If NOT EXISTS users (
+  id INT NOT NULL AUTO_INCREMENT,
+  lastname VARCHAR(45) NOT NULL,
+  firstname VARCHAR(45) NOT NULL,
+  username VARCHAR(45) NOT NULL,
+  password VARCHAR(45) NOT NULL,
+  PRIMARY KEY (id, username),
+  UNIQUE INDEX id_UNIQUE (id ASC) VISIBLE);
