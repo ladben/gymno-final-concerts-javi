e954baed37df106ba0faca1c699276c4c6d496cb
Ladányi Bence <ladanyi.bence96@gmail.com>
2021-09-13 16:02:09 +0200

    db migration changed because bcrypted password is over 45 chars

--- backend/migrations/sqls/20210913121858-add-users-up.sql
+++ backend/migrations/sqls/20210913121858-add-users-up.sql
@@ -3,6 +3,6 @@ CREATE TABLE If NOT EXISTS users (
   lastname VARCHAR(45) NOT NULL,
   firstname VARCHAR(45) NOT NULL,
   username VARCHAR(45) NOT NULL,
-  password VARCHAR(45) NOT NULL,
+  password VARCHAR(255) NOT NULL,
   PRIMARY KEY (id, username),
   UNIQUE INDEX id_UNIQUE (id ASC) VISIBLE);
