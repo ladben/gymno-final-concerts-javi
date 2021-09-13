312d86c17dbd69d803d168dd19a1215792fc1b01
Ladányi Bence <ladanyi.bence96@gmail.com>
2021-09-13 14:44:27 +0200

    heartbeat controller done

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,7 +2,7 @@ import logger from './logger';
 import app from './app';
 import config from './config';
 
-const PORT = config.port || 1234;
+const PORT = config.port || 5000;
 
 app.listen(PORT, () => {
   logger.info(`App is listening on ${PORT}`);
