c2b7842cf7612d5918945fd723defdbaed4655ae
Ladányi Bence <ladanyi.bence96@gmail.com>
2021-09-13 14:50:19 +0200

    interface import shortened

--- backend/src/data/connection.ts
+++ backend/src/data/connection.ts
@@ -1,7 +1,7 @@
 import mysql from 'mysql';
 
 import config from '../config';
-import { DbResult } from '../models/data/db-result';
+import { DbResult } from '../models';
 
 export const pool = mysql.createPool({
   connectionLimit: 2,
